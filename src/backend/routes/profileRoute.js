a8d0d822988cf419b8e772929cc75f31cf5997c2
Rishab <rishabdev.sidhu2@mail.dcu.ie>
2024-01-17 21:54:46 +0000

    updated profileRoute to add id to url

--- src/backend/routes/profileRoute.js
+++ src/backend/routes/profileRoute.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const voterController = require('../controllers/{{FILENAME}}');
 
 router.get('/api/user', {{FILENAME}}.userInfo);
-router.post('/api/useremail/:string', {{FILENAME}}.changeUserEmail)
-router.post('/api/usernumber/:string', {{FILENAME}}.changeUserNumber)
+router.post('/api/useremail/:id/:string', {{FILENAME}}.changeUserEmail)
+router.post('/api/usernumber/:id/:string', {{FILENAME}}.changeUserNumber)
 
 module.exports = router;
