adadf68a7d2e6c7a8a2c5b941a7c540e9449495e
Rishab <rishabdev.sidhu2@mail.dcu.ie>
2024-01-18 19:02:23 +0000

    removed connection to mysql, should be done through sequelize.js

--- src/backend/server.js
+++ src/backend/server.js
@@ -1,28 +1,10 @@
 const http = require('http');
-const mysql = require('mysql');
 const app = require('./app');
 
 const port = process.env.PORT || 3000;
 app.set('port', port);
 
 const server = http.createServer(app);
-
-const connection = mysql.createConnection({
-    host: 'your_mysql_host',
-    user: 'your_mysql_user',
-    password: 'your_mysql_password',
-    database: 'your_mysql_database',
-});
-
-connection.connect((err) => {
-    if (err) {
-        console.error('Error connecting to MySQL:', err);
-        return;
-    }
-    console.log('Connected to MySQL database');
-});
-  
-  
 
 server.listen(port, () => {
   console.log(`Server is listening on port ${port}`);
