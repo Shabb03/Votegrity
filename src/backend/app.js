90b4a69e2c01595eed29cf15f12163061661db4f
Rishab <rishabdev.sidhu2@mail.dcu.ie>
2024-01-19 21:51:23 +0000

    updated app.js with new routes, user, admin, election which will be split further through routes directory

--- src/backend/app.js
+++ src/backend/app.js
@@ -1,16 +1,10 @@
 const express = require('express');
 const passport = require('passport');
 
-//const addCandidateRoute = require('./routes/addCandidateRoute');
-//const addElectionRoute = require('./routes/addElectionRoute');
-//const dashboardRoute = require('./routes/dashboardRoute');
-//const loginRoute = require('./routes/loginRoute');
-//const profileRoute = require('./routes/profileRoute');
-const registerRoute = require('./routes/registerRoute');
-//const resetPasswordRoute = require('./routes/resetPasswordRoute');
-//const resetRoute = require('./routes/resetRoute');
-//const voteRoute = require('./routes/voteRoute');
-//const winnerRoute = require('./routes/winnerRoute');
+//const registerRoute = require('./routes/registerRoute');
+const userRoute = require('./routes/userRoute');
+const adminRoute = require('./routes/adminRoute');
+const electionRoute = require('./routes/electionRoute');
 
 const app = express();
 app.use(express.json());
@@ -19,7 +13,10 @@ app.get('/', (req, res) => {
     res.send('Hello World');
 });
 
-app.use('/api/register', registerRoute);
+//app.use('/api/register', registerRoute);
+app.use('/api/user', userRoute);
+app.use('/api/admin', adminRoute);
+app.use('/api/election', electionRoute);
 
 /*
 app.use('/', routes);
