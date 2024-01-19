6887317ae71b3e81ee55cb6e4a164bf3abf512d0
Rishab <rishabdev.sidhu2@mail.dcu.ie>
2024-01-19 15:51:06 +0000

    updated app.js with routes and tested server.js to listen on port 3000, successful

--- src/backend/app.js
+++ src/backend/app.js
@@ -1,18 +1,30 @@
 const express = require('express');
 const passport = require('passport');
-const routes = require('./routes');
-const app = express();
-
-app.use(express.json());
 
 /*
+const addCandidateRoute = require('./routes/addCandidateRoute');
+const addElectionRoute = require('./routes/addElectionRoute');
+const dashboardRoute = require('./routes/dashboardRoute');
+const loginRoute = require('./routes/loginRoute');
+const profileRoute = require('./routes/profileRoute');
+const registerRoute = require('./routes/registerRoute');
+const resetPasswordRoute = require('./routes/resetPasswordRoute');
+const resetRoute = require('./routes/resetRoute');
+const voteRoute = require('./routes/voteRoute');
+const winnerRoute = require('./routes/winnerRoute');
+*/
+
+const app = express();
+app.use(express.json());
+
 app.get('/', (req, res) => {
     res.send('Hello World');
 });
-*/
 
+/*
 app.use('/', routes);
 app.use(passport.initialize());
+*/
 
 // Handle 404 errors
 app.use((req, res) => {
