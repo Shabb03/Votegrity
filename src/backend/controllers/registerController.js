1dfd700bb8eca777aaee509acbd33b4b006079ac
Rishab <rishabdev.sidhu2@mail.dcu.ie>
2024-01-21 13:52:05 +0000

    added security questions and answers, and added password security requirements

--- src/backend/controllers/registerController.js
+++ src/backend/controllers/registerController.js
@@ -3,13 +3,28 @@ const { Voter } = require('../sequelize');
 
 const saltRounds = 10;
 
+function isSecurePassword(password) {
+    const hasNumber = /\d/.test(password);
+    const hasLowercase = /[a-z]/.test(password);
+    const hasUppercase = /[A-Z]/.test(password);
+    const hasSpecialChar = /[!@#$%^&*()_+{}\[\]:;<>,.?~\\/-]/.test(password);
+    return hasNumber && hasLowercase && hasUppercase && hasSpecialChar;
+}
+  
+
 exports.signup = async (req, res) => {
     try {
-        const { name, email, password, dateOfBirth, specialNumber, citizenship, phoneNumber } = req.body;
+        const { name, email, password, dateOfBirth, specialNumber, citizenship, phoneNumber, securityQuestion1, securityAnswer1, securityQuestion2, securityAnswer2 } = req.body;
         const existingUser = await Voter.findOne({ where: { email } });
         if (existingUser) {
             return res.status(400).json({ message: 'User already exists' });
         }
+
+        const isSecure = isSecurePassword(password);
+        if (!isSecure) {
+            res.send({message: 'Password is not strong enough'});
+        }
+
         const hashedPassword = await bcrypt.hash(password, saltRounds);
         const newUser = await Voter.create({
             name,
@@ -21,6 +36,10 @@ exports.signup = async (req, res) => {
             phoneNumber,
             authenticated: false,
             voted: false,
+            securityQuestion1, 
+            securityAnswer1, 
+            securityQuestion2, 
+            securityAnswer2
         });
 
         const userResponse = {
