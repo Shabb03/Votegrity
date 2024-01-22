963177c10a2ccb805797a8c2eb04fb950585dfa1
Rishab <rishabdev.sidhu2@mail.dcu.ie>
2024-01-22 15:41:22 +0000

    added authenticateToken middleware to electionRoute.js

--- src/backend/routes/electionRoute.js
+++ src/backend/routes/electionRoute.js
@@ -1,12 +1,13 @@
 const express = require('express');
 const router = express.Router();
+const authenticateToken = require('../middleware/authenticate');
 
 const voteController = require('../controllers/voteController');
 const winnerController = require('../controllers/winnerController');
 
-router.get('/candidates', voteController.getAllCandidates)
-router.post('/vote', voteController.submitVote);
+router.get('/candidates', authenticateToken, voteController.getAllCandidates)
+router.post('/vote', authenticateToken, voteController.submitVote);
 
-router.get('/results', winnerController.getResults);
+router.get('/results', authenticateToken, winnerController.getResults);
 
 module.exports = router;
