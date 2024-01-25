93871acf0d187c3589aa9fb4e97462e54762c3aa
Rishab <rishabdev.sidhu2@mail.dcu.ie>
2024-01-25 20:12:34 +0000

    updated router component names, receiving an error that all .vue components should be multi-worded

--- src/frontend/votegrity/src/router/router.js
+++ src/frontend/votegrity/src/router/router.js
@@ -1,31 +1,31 @@
 import { createRouter, createWebHistory } from 'vue-router';
-import Register from '@/views/Register.vue'
-//import Login from '@/views/Login.vue'
-//import Authentication from '@/views/Authentication.vue'
-//import Profile from '@/views/Profile.vue'
-//import Vote from '@/views/Vote.vue'
+import UserRegister from '@/views/UserRegister.vue'
+//import UserLogin from '@/views/UserLogin.vue'
+//import UserAuthentication from '@/views/UserAuthentication.vue'
+//import UserProfile from '@/views/UserProfile.vue'
+//import VoteCandidate from '@/views/VoteCandidate.vue'
 //import ResetPassword from '@/views/ResetPassword.vue'
 //import ChangePassword from '@/views/ChangePassword.vue'
 //import ThankYou from '@/views/ThankYou.vue'
-//import Results from '@/views/Results.vue'
+//import ElectionResults from '@/views/ElectionResults.vue'
 //import AdminLogin from '@/views/AdminLogin.vue'
 //import AdminDashboard from '@/views/AdminDashboard.vue'
 //import CreateElection from '@/views/CreateElection.vue'
 //import AddCandidate from '@/views/AddCandidate.vue'
-//import Reset from '@/views/Reset.vue'
+//import ResetElection from '@/views/ResetElection.vue'
 
 const router = createRouter({
     history: createWebHistory(),
     routes: [
-        { path: '/register', component: Register, name: 'Register' },
-        //{ path: '/login', component: Login, name: 'Login' },
-        //{ path: '/authentication', component: Authentication, name: 'Authentication' },
-        //{ path: '/profile', component: Profile, name: 'Profile' },
-        //{ path: '/vote', component: Vote, name: 'Vote' },
+        { path: '/register', component: UserRegister, name: 'Register' },
+        //{ path: '/login', component: UserLogin, name: 'Login' },
+        //{ path: '/authentication', component: UserAuthentication, name: 'Authentication' },
+        //{ path: '/profile', component: UserProfile, name: 'Profile' },
+        //{ path: '/vote', component: VoteCandidate, name: 'Vote' },
         //{ path: '/resetpassword', component: ResetPassword, name: 'Enter Code' },
         //{ path: '/changepassword', component: ChangePassword, name: 'Enter New Password' },
         //{ path: '/thankyou', component: ThankYou, name: 'Thank You' },
-        //{ path: '/results', component: Results, name: 'Results' },
+        //{ path: '/results', component: ElectionResults, name: 'Results' },
         //{ path: '/admin/login', component: AdminLogin, name: 'Login' },
         //{ path: '/admin/dashboard', component: AdminDashboard, name: 'Dashboard' },
         //{ path: '/admin/createelection', component: CreateElection, name: 'Create Election' },
