05dea79f2f5951cf41101c1e81e6bc2f636fc87a
Rishab <rishabdev.sidhu2@mail.dcu.ie>
2024-02-05 12:42:37 +0000

    Added vuetil and jest test tools for vue js app testing, pushing to test with git pipeline

---
+++ src/frontend/votegrity/src/__tests__/NotFound.test.js
@@ -0,0 +1,35 @@
+//import { shallowMount } from '@vue/test-utils';
+import { mount } from '@vue/test-utils';
+import NotFound from '../views/NotFound.vue';
+
+const mockRoute = {
+    name: 'mockedRouteName',
+};
+
+const VBtn = {
+    template: '<div></div>',
+};
+
+global.mocks = {
+    $vuetify: {
+      theme: {},
+    },
+    VBtn,
+  };
+
+describe('NotFound View Test', () => {
+  it('renders correctly', () => {
+    const wrapper = mount(NotFound, {
+        global: {
+          mocks: {
+            $route: mockRoute,
+          },
+          components: {
+            'v-btn': VBtn
+          },
+        },
+      });
+  
+      expect(wrapper.exists()).toBe(true);
+  });
+});
--- src/frontend/votegrity/src/__tests__/TestComponent.test.js
+++
@@ -1,9 +0,0 @@
-import { mount } from "@vue/test-utils";
-import NavigationBar from "@/components/navbar/NavigationBar.vue";
-
-describe("NavigationBar", () => {
-  it("renders properly", () => {
-    const wrapper = mount(NavigationBar);
-    //expect(wrapper.html()).toMatchSnapshot();
-  });
-});
