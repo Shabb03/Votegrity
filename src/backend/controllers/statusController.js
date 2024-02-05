4fa48799948577d74d4a9f2231a4072977a92887
Rishab <rishabdev.sidhu2@mail.dcu.ie>
2024-02-05 16:07:35 +0000

    Updated statusController, fixed error with jwt.verify throwing an error with function verifyToken()

--- src/backend/controllers/statusController.js
+++ src/backend/controllers/statusController.js
@@ -1,7 +1,17 @@
-const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
-//const passport = require('passport');
-const { Admin, Voter, Election } = require('../sequelize');
+const { Voter, Election } = require('../sequelize');
+require('dotenv').config();
+const secretKey = process.env.SECRET_KEY
+const adminSecretKey = process.env.ADMIN_SECRET_KEY
+
+//Wrong jwt verification throws an error so this function prevents throwing an error while verifying whether the token is valid or not
+const verifyToken = (token, key) => {
+    try {
+        return jwt.verify(token, key);
+    } catch (error) {
+        return null;
+    }
+};
 
 //Get the current status of the user in the system, logged in, authenticated, is an admin
 exports.getStatus = async (req, res) => {
@@ -11,24 +21,23 @@ exports.getStatus = async (req, res) => {
             return res.send({ status: 'Not Logged in', loggedIn: false});
         }
         const token = tokenString.replace("Bearer ", "");
-        const acessUser = jwt.verify(token, secretKey);
-        req.user = acessUser;
-        const userId = req.user.id;
-        const admin = await Admin.findByPk(userId);
+
+        const admin = verifyToken(token, adminSecretKey);
         if (admin) {
             const activeElection = await Election.findOne({where: {isActive: true}});
             return res.send({status: 'Admin', admin: true, election: activeElection});
         }
-        const user = await Voter.findByPk(userId);
+
+        const accessUser = verifyToken(token, secretKey);
+        req.user = accessUser;
+        const user = await Voter.findOne({ where: { email: req.user.email } });
         if (user) {
             const authenticated = user.authenticated
             return res.send({status: 'Not Authenticated', loggedIn: true, authenticated: authenticated});
         }
-        else {
-            return res.send({status: 'Not Logged in', loggedIn: false, wrongToken: true });
-        }
+        return res.send({status: 'Not Logged in', loggedIn: false, wrongToken: true });
     } 
     catch (error) {
-        res.status(500).send({error: 'An error has occured trying to log in'})
+        res.status(500).send({error: 'An error has occured getting the status of the user'})
     }
 }
