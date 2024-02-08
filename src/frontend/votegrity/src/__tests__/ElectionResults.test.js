a3aa55a39163f9436d592508ec23979eaf33d3f4
Rishab <rishabdev.sidhu2@mail.dcu.ie>
2024-02-08 15:53:03 +0000

    Added ElectionResults.test.js test file to check if ElectionResults view renders properly

---
+++ src/frontend/votegrity/src/__tests__/ElectionResults.test.js
@@ -0,0 +1,54 @@
+//import { shallowMount } from '@vue/test-utils';
+import { mount } from '@vue/test-utils';
+import AdminDashboard from '../views/AdminDashboard.vue';
+
+const mockRoute = {name: 'mockedRouteName',};
+
+const VBtn = {template: '<div></div>',};
+const VAppBar = {template: '<div></div>',};
+const VCol = {template: '<div></div>',};
+const VCard = {template: '<div></div>',};
+const VCardText = {template: '<div></div>',};
+const VCardTitle = {template: '<div></div>',};
+const VImg = {template: '<div></div>',};
+const VContainer = {template: '<div></div>',};
+const VRow = {template: '<div></div>',};
+
+global.mocks = {
+    $vuetify: {
+        theme: {},
+    },
+    VBtn,
+    VAppBar,
+    VCol,
+    VCard,
+    VCardText,
+    VCardTitle,
+    VImg,
+    VContainer,
+    VRow,
+};
+
+describe('AdminDashboard View Test', () => {
+    it('renders correctly', () => {
+        const wrapper = mount(AdminDashboard, {
+            global: {
+                mocks: {
+                    $route: mockRoute,
+                },
+                components: {
+                    'v-btn': VBtn,
+                    'v-app-bar': VAppBar,
+                    'v-col': VCol,
+                    'v-card': VCard,
+                    'v-card-text': VCardText,
+                    'v-card-title': VCardTitle,
+                    'v-img': VImg,
+                    'v-container': VContainer,
+                    'v-row': VRow
+                },
+            },
+        });
+        expect(wrapper.exists()).toBe(true);
+    });
+});
