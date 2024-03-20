938488803b52ea530dc63efbc10d47a4023dddce
Rishab <rishabdev.sidhu2@mail.dcu.ie>
2024-03-20 19:13:50 +0000

    Adding two new voting processes to voteController and VoteContainer.js, ranking and points-based

--- src/backend/controllers/voteController.js
+++ src/backend/controllers/voteController.js
@@ -1,6 +1,7 @@
 const calculateAge = require('./functions/calculateAge');
 const { Voter, Candidate, Vote, Election } = require('../sequelize');
 const Sequelize = require('sequelize');
+const processes = require('../assets/process.json');
 
 const fs = require('fs');
 const path = require('path');
@@ -74,6 +75,7 @@ exports.getImage = async (req, res) => {
 
 //Submit a vote cast by the user
 exports.submitVote = async (req, res) => {
+    //Work on this later to synchronize with the blockchain network
     try {
         const userId = req.user.id;
         const user = await Voter.findByPk(userId);
@@ -81,15 +83,11 @@ exports.submitVote = async (req, res) => {
         if (!authenticatedUser) {
             return res.json({error: 'User is not authenticated', authenticated: false});
         }
-        //const electionType = req.body.type;
-        //if and else statements for election type
-
-        const { candidateId, electionId, type } = req.body;
-        if (!candidateId || !electionId) {
-            return res.json({error: 'No candidate or election selected'});
+        const electionType = req.body.type;
+        if (!processes.includes(electionType)) {
+            return res.json({error: 'Incorrect election type'});
         }
-
-        /*
+        const { electionId } = req.body;
         const election = Election.findByPk(electionId);
         const age = calculateAge(user.dateOfBirth);
         const email = user.email;
@@ -98,19 +96,59 @@ exports.submitVote = async (req, res) => {
         if (age < election.ageRestriction || (election.authEmail && emailDomain !== election.authEmail) || (election.authCitizenship && user.citizenship !== election.authCitizenship)) {
             return res.json({error: 'You do not meet the voting requirements'});
         }
-        */
 
-        console.log(candidateId, electionId, type);
-
-        //Work on this later to synchronize with the blockchain network
-
-        /*
-        const vote = await Vote.create({
-            voterId: userId,
-            candidateId: candidateId,
-            electionId: electionId,
-        });
-        */
+        //if and else statements for election type
+        if (electionType ===  processes[0]) {
+            //majority vote contract
+            const { candidateId } = req.body;
+            if (!candidateId || !electionId) {
+                return res.json({error: 'No candidate or election selected'});
+            }
+            const candidate = await Candidate.findByPk(candidateId);
+            if (!candidate) {
+                return res.json({error: 'Selected candidate not found'});
+            }
+            /*
+            const vote = await Vote.create({
+                voterId: userId,
+                candidateId: candidateId,
+                electionId: electionId,
+            });
+            */
+        }
+        else if (electionType === processes[1]) {
+            //ranking vote contract
+            const { ranks } = req.body;
+            if (!ranks) {
+                return res.json({error: 'Vote rankings not provided'});
+            }
+            const valueSet = new Set();
+            for (const key in ranks) {
+                const value = ranks[key];
+                if (valueSet.has(value)) {
+                    return res.json({error: 'Multiple candidates have the same ranking'});
+                } 
+                else {
+                    valueSet.add(value);
+                }
+            }
+        }
+        else if (electionType === processes[2]) {
+            //score based vote contract
+            const { scores } = req.body;
+            if (!scores) {
+                return res.json({error: 'Vote scores not provided'});
+            }
+            let sum = 100
+            for (const key in scores) {
+                const value = ranks[key];
+                sum -= value;
+            }
+            if (sum !== 0) {
+                return res.json({error: 'All available score points must be used'});
+            }
+        }
+        
         res.json({ message: 'Vote submitted successfully'});
     }
     catch (error) {
---
+++ src/frontend/src/components/inputs/RankChoiceInput.vue
@@ -0,0 +1,31 @@
+<template>
+    <v-sheet class="mx-auto" width="350">
+    <v-select
+        clearable
+        v-model="rank"
+        label="Choose Rank Preference"
+        :items="rankData"
+        :rules="rankRules"
+        @update:modelValue="updateRank"
+    ></v-select>
+    </v-sheet>
+</template>
+
+<script>
+export default {
+    props: {
+        rankData: Array,
+    },
+    data: () => ({
+        rank: null,
+        rankRules: [
+            v => !!v || 'rank is required',
+        ],
+    }),
+    methods: {
+        updateRank() {
+            this.$emit('update:rank', this.rank);
+        },
+    },
+};
+</script>
