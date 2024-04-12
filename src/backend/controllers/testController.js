d02f4491b8d06119be956127b36322d04f3fb51a
Rishab <rishabdev.sidhu2@mail.dcu.ie>
2024-04-12 21:33:14 +0100

    commented out contractABI as it always raises an error on git pipeline but cannot be avoided

--- src/backend/controllers/testController.js
+++ src/backend/controllers/testController.js
@@ -10,8 +10,7 @@ const { promisify } = require('util');
 const readFileAsync = promisify(fs.readFile);
 const BlindSignature = require('blind-signatures');
 
-const contractABI = require('../../blockchain/contract/artifacts/contracts/Vote.sol/Vote.json');
-//const contractAddress = '0x5FbDB2315678afecb367f032d93F642f64180aa3';
+//const contractABI = require('../../blockchain/contract/artifacts/contracts/Vote.sol/wrong/Vote.json');
 const contractAddress = process.env.CONTRACT_ADDRESS;
 
 const dotenv = require('dotenv');
